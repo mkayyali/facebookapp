af80bcb51cf3abbd36d9d7c2d0fb2767e5615905
mskayyali <salehmeka@gmail.com>
2014-03-08 20:38:45 +0300

    generate posts scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Facebookapp::Application.routes.draw do
   
+  resources :posts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
